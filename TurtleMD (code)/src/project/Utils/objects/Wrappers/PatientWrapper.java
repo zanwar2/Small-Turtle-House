a635a069a4db13036d1e6929af6aa8ad994dfdd3
Ziyad Anwar <zanwar2@lsu.edu>
2021-03-22 21:38:47 -0500

    Questionnaire basis
    Questionnaire coding began

--- TurtleMD (code)/src/project/Utils/objects/Wrappers/PatientWrapper.java
+++ TurtleMD (code)/src/project/Utils/objects/Wrappers/PatientWrapper.java
@@ -3,14 +3,11 @@ package project.Utils.objects.Wrappers;
 import project.Main;
 import project.Utils.storage.Queries;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PatientWrapper extends UserWrapper {
 
-    private Date next_appointment;
+    private Timestamp next_appointment;
     private Integer patient_id;
 
     public PatientWrapper(String username, String password){
@@ -25,7 +22,7 @@ public class PatientWrapper extends UserWrapper {
         this.patient_id = null;
     }
 
-    public PatientWrapper(String username, String password, String last_name, String first_name, Date next_appointment, int patient_id){
+    public PatientWrapper(String username, String password, String last_name, String first_name, Timestamp next_appointment, int patient_id){
         super(username,password,last_name,first_name);
         this.next_appointment = next_appointment;
         this.patient_id = patient_id;
@@ -39,7 +36,7 @@ public class PatientWrapper extends UserWrapper {
             stmt.setString(2,this.getPassword());
             stmt.setString(3,this.getLast_name());
             stmt.setString(4,this.getFirst_name());
-            stmt.setDate(5,this.next_appointment);
+            stmt.setTimestamp(5,this.next_appointment);
             stmt.execute();
             if(patient_id != null){
                 stmt = con.prepareStatement(Queries.ADD_PATIENT_ID);
---
+++ TurtleMD (code)/src/project/applications/controllers/patient/QuestionnaireController.java
@@ -0,0 +1,12 @@
+package project.applications.controllers.patient;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class QuestionnaireController {
+
+    @FXML
+    private TextField temperature;
+
+    @FXML
+}
