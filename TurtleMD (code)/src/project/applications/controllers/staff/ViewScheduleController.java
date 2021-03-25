b4647407a3301a5bca0a2f47ece860690402ecdc
Jesse Chang <chaje3@gmail.com>
2021-03-25 16:19:46 -0500

    ViewSchedule
    I added a DatePicker class as a placeholder in the ViewScheduleController in case if it can be change or removed or if there's an improved version of it (Disclaimer: this is not my code, I simply copy paste in stackoverflow)

--- TurtleMD (code)/src/project/applications/controllers/staff/ViewScheduleController.java
+++ TurtleMD (code)/src/project/applications/controllers/staff/ViewScheduleController.java
@@ -1,4 +1,78 @@
 package project.applications.controllers.staff;
 
+//IMPORTANT: THE DATE PICKER CLASS IS A PLACEHOLDER
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.DatePicker;
+import javafx.util.StringConverter;
+
 public class ViewScheduleController {
+    public class DateTimePicker extends DatePicker{
+
+        private ObjectProperty<LocalTime> timeValue = new SimpleObjectProperty<>();
+        private ObjectProperty<ZonedDateTime> dateTimeValue;
+
+        public DateTimePicker(){
+            super();
+            setValue(LocalDate.now());
+            setTimeValue(LocalTime.now());
+            setConverter(new StringConverter<LocalDate>() {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HH:mm:ssZ");
+                @Override
+                public String toString ( LocalDate object ) {
+                    return dateTimeValue.get().format(formatter);
+                }
+
+                @Override
+                public LocalDate fromString ( String string ) {
+                    return LocalDate.parse(string, formatter);
+                }
+            });
+        }
+
+        public LocalTime getTimeValue(){
+            return timeValue.get();
+        }
+
+        void setTimeValue(LocalTime timeValue){
+            this.timeValue.set(timeValue);
+        }
+
+        public ObjectProperty<LocalTime> timeValueProperty(){
+            return timeValue;
+        }
+
+
+        public ZonedDateTime getDateTimeValue() {
+            return dateTimeValueProperty().get();
+        }
+
+
+        public void setDateTimeValue (ZonedDateTime dateTimeValue) {
+            dateTimeValueProperty().set(dateTimeValue);
+        }
+
+        public ObjectProperty<ZonedDateTime> dateTimeValueProperty(){
+            if (dateTimeValue == null){
+                dateTimeValue = new SimpleObjectProperty<>(ZonedDateTime.of(LocalDateTime.of(this.getValue(), timeValue.get()), ZoneId.systemDefault()));
+                timeValue.addListener(t -> {
+                    dateTimeValue.set(ZonedDateTime.of(LocalDateTime.of(this.getValue(), timeValue.get()), ZoneId.systemDefault()));
+                });
+
+                valueProperty().addListener(t -> {
+                    dateTimeValue.set(ZonedDateTime.of(LocalDateTime.of(this.getValue(), timeValue.get()), ZoneId.systemDefault()));
+                });
+            }
+            return dateTimeValue;
+        }
+
+    }
 }
