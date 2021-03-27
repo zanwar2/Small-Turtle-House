70c5f8715bf0963a425c47b59d4e61c999c57cd1
Ziyad Anwar <zanwar2@lsu.edu>
2021-03-26 23:07:27 -0500

    Finished Date selection

--- TurtleMD (code)/src/project/applications/controllers/patient/SchedulingController.java
+++ TurtleMD (code)/src/project/applications/controllers/patient/SchedulingController.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import project.Main;
+import project.Utils.objects.Wrappers.PatientWrapper;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -40,31 +41,59 @@ public class SchedulingController
 
     private List<Button> button;
 
+    private List<Date> times;
+
     private Date date;
 
 
     public void setDates() {
         button = new ArrayList<>(Arrays.asList(new Button[]{Mon,Tue,Wed,Thur,Fri,Sat,Sun}));
+        times = new ArrayList<>();
+        int i =0;
         Date today = Date.from(Instant.now());
         for(Button btn : button) {
             today = new Date(today.getTime()+86400000);
+            times.add(today);
             btn.setText(today.toString());
+            i++;
         }
     }
 
     public void BackAction(MouseEvent mouseEvent) throws IOException {
-        Parent root = FXMLLoader.load(Main.class.getResource("applications/resources/fxml/patient/NextAppointment.fxml"));
-        Main.getPrimaryStage().setTitle("NextAppointment Screen");
-        Main.getPrimaryStage().setScene(new Scene(root, root.prefWidth(500), root.prefHeight(500)));
+        PatientWrapper user = (PatientWrapper) Main.getUserWrapper();
+        if(user.getNext_appointment() == null)
+        {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("applications/resources/fxml/patient/NextAppointment.fxml"));
+            Parent root = loader.load();
+            NextAppointmentController appointmentController = loader.getController();
+            appointmentController.getCancelBtn().setVisible(false);
+            appointmentController.setInfo("There is no appointment Scheduled");
+            appointmentController.setOptionText("Schedule New Appointment");
+            Main.getPrimaryStage().setTitle("Next Appointment Screen");
+            Main.getPrimaryStage().setScene(new Scene(root, root.prefWidth(500), root.prefHeight(500)));
+        }
+        else
+        {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("applications/resources/fxml/patient/NextAppointment.fxml"));
+            Parent root = loader.load();
+            NextAppointmentController appointmentController = loader.getController();
+            appointmentController.getCancelBtn().setVisible(true);
+            appointmentController.setInfo(user.getNext_appointment().toString());
+            appointmentController.setOptionText("Reschedule Appointment");
+            Main.getPrimaryStage().setTitle("Next Appointment Screen");
+            Main.getPrimaryStage().setScene(new Scene(root, root.prefWidth(500), root.prefHeight(500)));
+        }
     }
 
     public void DateAction(MouseEvent mouseEvent) throws IOException {
-        //CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS | CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS | CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS
-        /*&CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS*/ Date date = Date.from(Instant.now()); //CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS
-        //CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS | CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS | CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS
+        Button find = (Button) mouseEvent.getSource();
+        System.out.println(find.getId() + " qq");
+        Date date = times.get(button.indexOf(find));
+        System.out.println(date);
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("applications/resources/fxml/patient/TimeSelection.fxml"));
-        ((TimeSelectionController) loader.getController()).setDate(date.getTime());
         Parent root = loader.load();
+        TimeSelectionController timeController = loader.getController();
+        timeController.setDate(date.getTime());
         Main.getPrimaryStage().setTitle("Time Selection");
         Main.getPrimaryStage().setScene(new Scene(root, root.prefWidth(500), root.prefHeight(500)));
 
