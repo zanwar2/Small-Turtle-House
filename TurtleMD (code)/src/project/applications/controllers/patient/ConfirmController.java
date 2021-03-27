d0dc8a059d15fe7bf124f59086cebf28cef3d2d1
Ziyad Anwar <zanwar2@lsu.edu>
2021-03-27 14:47:44 -0500

    Confirm Yes button now takes you to nextappointment screen

--- TurtleMD (code)/src/project/applications/controllers/patient/ConfirmController.java
+++ TurtleMD (code)/src/project/applications/controllers/patient/ConfirmController.java
@@ -1,11 +1,15 @@
 package project.applications.controllers.patient;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import project.Main;
 import project.Utils.objects.QuestionnaireHandler;
 import project.Utils.objects.Wrappers.PatientWrapper;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 
@@ -18,12 +22,15 @@ public class ConfirmController {
         (((Node) event.getSource()).getScene().getWindow()).hide();
     }
 
-    public void yesBtnAction(MouseEvent event) throws SQLException {
+    public void yesBtnAction(MouseEvent event) throws SQLException,IOException {
         PatientWrapper wrapper =  (PatientWrapper) Main.getUserWrapper();
         wrapper.setNext_appointment(next_appointment);
+        System.out.println(wrapper.getNext_appointment());
         questionnaire.saveChanges();
         wrapper.saveChanges();
         (((Node) event.getSource()).getScene().getWindow()).hide();
+            Main.setNextAppointmentScreen();
+
     }
 
     public void setData(QuestionnaireHandler questionnaire, Timestamp next_appointment) {
