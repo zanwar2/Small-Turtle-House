291cc9c45084e0d4dcfb241422b2dde02550dfe1
IshyCommits <jacedw1@gmail.com>
2021-03-27 04:15:27 -0600

    changes didnt go through? weird, commit 2

--- TurtleMD (code)/src/project/applications/controllers/patient/NamesController.java
+++ TurtleMD (code)/src/project/applications/controllers/patient/NamesController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import project.Main;
