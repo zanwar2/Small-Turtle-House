5c715ccfe053fce24fa22de37f0ba19465224960
IshyCommits <jacedw1@gmail.com>
2021-03-26 22:35:44 -0600

    giving ziyad methods to use :)

--- TurtleMD (code)/src/project/applications/controllers/patient/SchedulingController.java
+++ TurtleMD (code)/src/project/applications/controllers/patient/SchedulingController.java
@@ -38,29 +38,35 @@ public class SchedulingController
     @FXML
     private Button Sun;
 
-    List<Button> button;
+    private List<Button> button;
+
+    private Date date;
 
 
-    public void setDates()
-    {
+    public void setDates() {
         button = new ArrayList<>(Arrays.asList(new Button[]{Mon,Tue,Wed,Thur,Fri,Sat,Sun}));
         Date today = Date.from(Instant.now());
-        for(Button btn : button)
-        {
+        for(Button btn : button) {
             today = new Date(today.getTime()+86400000);
             btn.setText(today.toString());
         }
     }
 
-    public void BackAction(MouseEvent mouseEvent) throws IOException
-    {
+    public void BackAction(MouseEvent mouseEvent) throws IOException {
         Parent root = FXMLLoader.load(Main.class.getResource("applications/resources/fxml/patient/NextAppointment.fxml"));
         Main.getPrimaryStage().setTitle("NextAppointment Screen");
         Main.getPrimaryStage().setScene(new Scene(root, root.prefWidth(500), root.prefHeight(500)));
     }
 
-    public void DateAction(MouseEvent mouseEvent)
-    {
+    public void DateAction(MouseEvent mouseEvent) throws IOException {
+        //CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS | CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS | CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS
+        /*&CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS*/ Date date = Date.from(Instant.now()); //CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS
+        //CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS | CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS | CHANGE THIS DATE TO THE DATE FROM THE BUTTON PRESS
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("applications/resources/fxml/patient/TimeSelection.fxml"));
+        ((TimeSelectionController) loader.getController()).setDate(date.getTime());
+        Parent root = loader.load();
+        Main.getPrimaryStage().setTitle("Time Selection");
+        Main.getPrimaryStage().setScene(new Scene(root, root.prefWidth(500), root.prefHeight(500)));
 
     }
 
--- TurtleMD (code)/src/project/applications/controllers/patient/TimeSelectionController.java
+++ TurtleMD (code)/src/project/applications/controllers/patient/TimeSelectionController.java
@@ -1,6 +1,13 @@
 package project.applications.controllers.patient;
 
+import java.sql.Date;
+
 public class TimeSelectionController
 {
+    private Date date;
+
+    public void setDate(long date) {
+        this.date = new Date(date);
+    }
 
 }
